5aede50d54147a8e9b331c34378c131ed7f1a381
SanaEl-shamy <snelshamy@gmail.com>
2022-04-08 18:13:32 +0200

    splash screen xml intro to app xml sign up and sign in xml forget and change password xml

---
+++ app/src/main/java/com/orange/e_learningcourses/IntroAppP3.java
@@ -0,0 +1,14 @@
+package com.orange.e_learningcourses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroAppP3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_app_p3);
+    }
+}
---
+++ app/src/main/java/com/orange/e_learningcourses/VerificationCode.java
@@ -0,0 +1,14 @@
+package com.orange.e_learningcourses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerificationCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verification_code);
+    }
+}
